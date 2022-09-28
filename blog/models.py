2534fb8f7e9cf70e2648412ce0ea1c54499da797
yildirimkadir <yildirimkad34@gmail.com>
2022-09-28 22:08:52 +0200

    "Das Projekt und models.py wurden erstellt."

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+    def __str__(self):
+        return self.name
+    
+
+
+class Post(models.Model):
+    OPTIONS = (
+        ("d", "Draft"),
+        ("p", "Published"),
+    )
+    
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    status = models.CharField(max_length=10, choices= OPTIONS, default = "d")
+    slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
