140680e311c2e8e79e04e956c045d07ec5aed279
yildirimkadir <yildirimkad34@gmail.com>
2022-10-01 20:27:31 +0200

    "templates wurde erstellt."

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import post_list
 
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-# ]
+urlpatterns = [
+    path("", post_list, name='list'	),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "queryset": queryset
+    }
+    return render(request, "blog/post_list.html", context)
