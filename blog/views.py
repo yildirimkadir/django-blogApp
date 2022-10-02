7ca75406bf48700c71864706eded614794357f6a
yildirimkadir <yildirimkad34@gmail.com>
2022-10-02 14:37:53 +0200

    "views.py wurde aktualisiert."

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     queryset = Post.objects.all()
@@ -7,3 +9,17 @@ def post_list(request):
         "queryset": queryset
     }
     return render(request, "blog/post_list.html", context)
+
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("blog:list")
+    context = {
+        "form": form
+    }
+    return render(request, "blog/post_create.html", context)
