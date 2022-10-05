f05ae342f13665d95e1e623d886fa3b8f5e8215c
yildirimkadir <yildirimkad34@gmail.com>
2022-10-05 17:35:41 +0200

    "post_delete wurde hinzugefügt."

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import post_list, post_create, post_detail
+from .views import post_list, post_create, post_detail, post_update, post_delete
 
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name='list'	),
     path("create/", post_create, name='create'),
     path("<str:slug>/", post_detail, name='detail'),
+    path("update/<str:slug>/", post_update, name='update'),
+    path("delete/<str:slug>/", post_delete, name='delete'),
 ]
