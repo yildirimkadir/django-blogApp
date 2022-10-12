ba1317a848f46082017639326f7e516a361b2745
yildirimkadir <yildirimkad34@gmail.com>
2022-10-12 19:02:27 +0200

    "Message-Eigenschaft wurde hinzugefügt."

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post, Like
 from .forms import CommentForm, PostForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def post_list(request):
@@ -22,6 +23,7 @@ def post_create(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            messages.success(request,"New Post created successfully")
             return redirect("blog:list")
     context = {
         "form": form
@@ -46,13 +48,15 @@ def post_detail(request, slug):
     }
     return render(request, "blog/post_detail.html", context)
 
+@login_required() #--> bunu url e update yazdigimda login e yönlendirmesi icin yazdim
 def post_update(request, slug):
     obj= get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=obj)
     if request.user.id != obj.author.id:
-        return HttpResponse("You're not authorized!!")
+        messages.warning(request, "You're not authorized!!")
     if form.is_valid():
         form.save()
+        messages.success(request,"Post updated successfully")
         return redirect("blog:list")
     context = {
         "object": obj,
@@ -66,12 +70,14 @@ def post_delete(request, slug):
         return HttpResponse("You're not authorized!!")
     if request.method=='POST':
         obj.delete()
+        messages.warning(request, "Post deleted!")
         return redirect("blog:list")
     context = {
         "object": obj,
     }
     return render(request, "blog/post_delete.html", context)
 
+@login_required() #--> bunu detail sayfasinda like verebilmek icin login e yönlendirmesi icin yazdim.Login olmadan like yapamamali kullanici.
 def like(request, slug):
     if request.method == "POST":
         object = get_object_or_404(Post, slug=slug)
@@ -81,4 +87,4 @@ def like(request, slug):
         else:
             Like.objects.create(user=request.user, post=object)
         return redirect("blog:detail", slug=slug)
-    return redirect("blog:detail", slug=slug)
+    return redirect("blog:detail", slug=slug) #--> bunu method=="GET" oldugunda redirect etmek icin yazdim .. 
